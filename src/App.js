8972e3afecbb2a91d1cfb413fc36bf06dc84eb2c
Teja Kumar M <tejakumarm13@gmail.com>
2024-12-09 17:01:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo}></img>
+        <h3>Teja Kumar M</h3>
+        <div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
