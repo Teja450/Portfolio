12e482c2cb7e822951a00ff03d9e95d8c8b57a52
Teja Kumar M <tejakumarm13@gmail.com>
2024-12-11 14:33:13 +0530

    Fixed ESLint errors and updated dependencies

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Home from './Components/Home';
-import Navbar from './Components/Navbar'; // Import Navbar component
+// import Navbar from './Components/Navbar'; // Import Navbar component
 import Footer from './Components/Footer';
 import Projects from './Components/Projects'; // Create Projects Component
 import ContactForm from './Components/ContactForm'; // Create ContactForm Component
--- Client/client/src/Components/Footer.js
+++ Client/client/src/Components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>© 2024 Teja Kumar M. All Rights Reserved.</p>
+        <p>2024 Teja Kumar M.</p>
         <div className="social-links">
-          <a href="#" className="social-icon">Instagram</a>
-          <a href="#" className="social-icon">Twitter</a>
-          <a href="#" className="social-icon">LinkedIn</a>
+          <a href="https://www.instagram.com/teja_km134/profilecard/?igsh=eG9udW8xYmgyYnpy" target="_blank" rel="noreferrer" className="social-icon">Instagram</a>
+          <a href="https://x.com/TejaKumar36135" target="_blank" rel="noreferrer" className="social-icon">Twitter</a>
+          <a href="https://www.linkedin.com/in/teja-kumar-m-0727b1258?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"  target="_blank" rel="noreferrer" className="social-icon">LinkedIn</a>
         </div>
       </div>
     </footer>
